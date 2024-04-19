20f21895c455dbdf50ce00a7e72449ae6baad077
John Previtera <johngprevitera@gmail.com>
2024-04-19 11:30:27 -0600

    posts looking good

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -11,16 +11,24 @@ defineProps({ post: { type: Post, required: true } })
 
 <template>
     <section class="row card m-4 shadow">
-        <section class="col-10 p-4">
+        <section class="col-12 p-4 ">
             <section class="row">
                 <div class="col-2">
                     <img :src="post.creator.picture" :alt="post.creator.name" :title="post.creator.name"
                         class="img-fluid user-img">
-                    <h3>{{ post.creator.name }}</h3>
-                    <p>{{ post.createdAt.toLocaleDateString }}</p>
-
                 </div>
-                <section class="col">{{ post.body }}</section>
+                <section class="col">
+                    <h3>{{ post.creator.name }}</h3>
+                    <div class="row text-center">
+                        <p class="col-2 m-0 p-0">{{ post.createdAt.toLocaleDateString() }}</p>
+                        <p v-if="post.creator.graduated" class="col text-start m-0 p-0"><i
+                                class="mdi mdi-account-school"></i>
+                        </p>
+                    </div>
+                </section>
+            </section>
+            <section class="row mt-3">
+                <div class="col-12 fs-5">{{ post.body }}</div>
             </section>
 
         </section>
